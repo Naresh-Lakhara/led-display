13aef45bca1fb4a67df70e65379676481827b717
Naresh Lakhara <41575133+Naresh-Lakhara@users.noreply.github.com>
2025-08-09 15:38:58 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const bodyParser = require("body-parser");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+let latestMessage = "Welcome!";
+
+app.post("/send", (req, res) => {
+    latestMessage = req.body.message;
+    io.emit("update", latestMessage);
+    res.json({ status: "ok" });
+});
+
+io.on("connection", (socket) => {
+    socket.emit("update", latestMessage);
+});
+
+server.listen(3000, () => console.log("Server running on port 3000"));
